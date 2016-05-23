4fbefda3c9593497b28564f43fb52a9252f70cd6
Charrey <p.vanleeuwen@student.utwente.nl>
2016-05-23 11:57:59 +0200

    cfrgbottomupbuilderexercise4ccmaandag

--- src/pp/block4/cc/cfg/BottomUpCFGBuilder.java
+++ src/pp/block4/cc/cfg/BottomUpCFGBuilder.java
@@ -23,10 +23,9 @@ public class BottomUpCFGBuilder extends FragmentBaseListener {
 	/** The CFG being built. */
 	private Graph graph;
 
-	private Node entry;
-	private Node exit;
 
-	//ParseTreeProperty
+	ParseTreeProperty<Node> entrie;
+	ParseTreeProperty<Node> exitie;
 
 	/** Builds the CFG for a program contained in a given file. */
 	public Graph build(File file) {
@@ -58,17 +57,78 @@ public class BottomUpCFGBuilder extends FragmentBaseListener {
 
 	/** Builds the CFG for a program given as an ANTLR parse tree. */
 	public Graph build(ParseTree tree) {
+		entrie = new ParseTreeProperty<>();
 		this.graph = new Graph();
-
 		new ParseTreeWalker().walk(this, tree);
+		return this.graph;
+	}
 
+	@Override
+	public void exitWhileStat(@NotNull FragmentParser.WhileStatContext ctx) {
+		Node mynode = addNode(ctx, "WHILE");
+		entrie.put(ctx, mynode);
+		mynode.addEdge(entrie.get(ctx.getChild(4)));
+		exitie.get(ctx.getChild(4)).addEdge(mynode);
+		exitie.put(ctx, mynode);
+	}
 
-		return null;
-		//throw new UnsupportedOperationException("Fill in");
+	@Override
+	public void exitDecl(@NotNull FragmentParser.DeclContext ctx) {
+		Node mynode = addNode(ctx, "Decl");
+		entrie.put(ctx, mynode);
+		exitie.put(ctx, mynode);
+	}
+
+	@Override
+	public void exitAssignStat(@NotNull FragmentParser.AssignStatContext ctx) {
+		Node mynode = addNode(ctx, "Assign");
+		entrie.put(ctx, mynode);
+		exitie.put(ctx, mynode);
+	}
+
+	@Override
+	public void exitBlockStat(@NotNull BlockStatContext ctx) {
+		if (ctx.getChildCount()==2) {
+			Node mynode = addNode(ctx, "Empty_Block");
+			entrie.put(ctx, mynode);
+			exitie.put(ctx, mynode);
+		} else {
+			entrie.put(ctx, entrie.get(ctx.getChild(1)));
+			exitie.put(ctx, exitie.get(ctx.getChild(ctx.getChildCount()-2)));
+			for (int i = 1; i<ctx.getChildCount()-2; i++) {
+				exitie.get(ctx.getChild(i)).addEdge(entrie.get(ctx.getChild(i+1)));
+			}
+		}
+	}
+
+	@Override
+	public void exitPrintStat(@NotNull FragmentParser.PrintStatContext ctx) {
+		Node mynode = addNode(ctx, "Print");
+		entrie.put(ctx, mynode);
+		exitie.put(ctx, mynode);
 	}
 
 	@Override
 	public void exitIfStat(@NotNull FragmentParser.IfStatContext ctx) {
+			Node mynode = addNode(ctx, "IF");
+			if (ctx.getChildCount()==5) {
+				entrie.put(ctx, mynode);
+				mynode.addEdge(entrie.get(ctx.getChild(4)));
+				Node exitnode = addNode(ctx, "JOIN");
+				mynode.addEdge(exitnode);
+				exitie.get(ctx.getChild(4)).addEdge(exitnode);
+				exitie.put(ctx, exitnode);
+			} else if (ctx.getChildCount()==7) {
+				entrie.put(ctx, mynode);
+				mynode.addEdge(entrie.get(ctx.getChild(4)));
+				mynode.addEdge(entrie.get(ctx.getChild(6)));
+				Node exitnode = addNode(ctx, "JOIN");
+				exitie.get(ctx.getChild(4)).addEdge(exitnode);
+				exitie.get(ctx.getChild(6)).addEdge(exitnode);
+				exitie.put(ctx, exitnode);
+			} else {
+				System.err.println("WAT NO MUNNIE");
+			}
 
 	}
 
